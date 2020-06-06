73eb5ea3a2a666382781830a5b5dbc26a73c53b3
honshin4n1 <million_films.sh@icloud.com>
2020-06-06 10:55:16 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+# secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
